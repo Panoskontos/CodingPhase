1c2727a24ab32166e387b348fa4dc843edf80037
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-06 01:46:44 +0200

    javascript first lesson

--- Future Proof Javascript/main.js
+++ Future Proof Javascript/main.js
@@ -18,3 +18,26 @@ console.log(Car)
 
 // you don't need the semi column
 // it is good practice for statement finishing
+
+
+// Javascript Data Types
+
+// Find type of element
+console.log(typeof apples);
+console.log(typeof car);
+console.log(typeof total);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof [1,2,3,4]);
+console.log(typeof function(){});
+
+// You can cast the variable however you want 
+// nothing crashes
+var total = 32.4512
+total = 'hello'
+total = undefined
+toatl = [1,2,3,4]
+total = false
+
+
+
