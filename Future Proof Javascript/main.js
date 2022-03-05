c3ea429786612710f1f0c5f30cc72d011055f17a
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-06 01:36:37 +0200

    javascript first lesson

--- Future Proof Javascript/main.js
+++ Future Proof Javascript/main.js
@@ -1,8 +1,20 @@
-var car = 'honda civic';
-var car = 'nissan';
-
-var box = ' this is a box';
+var car = 'nissan'
+var apples = 5
+var box = 'this is a box'
 
 console.log(car)
 console.log(box)
+console.log(apples*200)
 
+/*
+This comments will not be printed
+*/
+
+
+// Javascript is case Sensitive
+var car = 'honda civic'
+var Car = 'BMW'
+console.log(Car)
+
+// you don't need the semi column
+// it is good practice for statement finishing
