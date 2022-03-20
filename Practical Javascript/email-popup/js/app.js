b35ec314f94b2dc91bc474cce2af65bd39706586
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-21 00:37:00 +0200

    new

--- Practical Javascript/email-popup/js/app.js
+++ Practical Javascript/email-popup/js/app.js
@@ -5,14 +5,19 @@ var modal = document.querySelector('.email-modal')
 
 // console.log(decline_button)
 
+let emailState = false;
 // show modal everytime you leave the page
 // anonymous function
 window.onload = function() {
     // arrow function
-    document.addEventListener('mouseleave', () => {
-        modal.classList.add('email-modal--visible')
-    })
-}
+    if (emailState == false){
+        document.body.addEventListener('mouseleave', () => {
+            modal.classList.add('email-modal--visible')
+        })
+    }
+} 
+
+    
 
 
 // Fixed Decline button
@@ -23,8 +28,16 @@ decline_button.onclick = function(){
 
 // Close Button
 closeButton = document.querySelector('.email-modal__close-btn')
+closeButton.style.background = 'red'
+closeButton.style.borderRadius = '10px'
+
 closeButton.onclick = function(){
     console.log('yeah')
     var modal = document.querySelector('.email-modal')
     modal.classList.remove('email-modal--visible')
+    console.log(count)
+
 }
+
+// Check if close button has been clicked before
+
