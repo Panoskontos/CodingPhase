efe1c12311e6857277d3cdcafe4a9ab359057376
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-21 00:12:32 +0200

    new

--- Practical Javascript/email-popup/js/app.js
+++ Practical Javascript/email-popup/js/app.js
@@ -1,17 +1,20 @@
 console.log('hi')
 
 decline_button = document.querySelector('.email-modal__decline-offer')
+var modal = document.querySelector('.email-modal')
 
 // console.log(decline_button)
 
 // show modal everytime you leave the page
-console.log(window)
+// anonymous function
+window.onload = function() {
+    modal.classList.add('email-modal--visible')
+}
 
 
 // Fixed Decline button
 decline_button.onclick = function(){
     console.log('yeah')
-    var modal = document.querySelector('.email-modal')
     modal.classList.remove('email-modal--visible')
 }
 
