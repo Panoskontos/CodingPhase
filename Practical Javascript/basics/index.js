6ad0ca647fb75ffa32ad1813d15d7b46b5a602be
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-20 23:00:08 +0200

    new

--- Practical Javascript/basics/index.js
+++ Practical Javascript/basics/index.js
@@ -3,3 +3,13 @@ console.log('hi')
 document.body.style.background = 'pink'
 document.body.style.margin = 0
 document.body.style.padding = 0
+
+// using js instead of html css
+let div = document.createElement('div') 
+div.className = "js-box"
+div.style.width = '300px'
+div.style.height = '200px'
+div.style.background = 'black'
+div.style.color = "white"
+div.innerHTML = "<p> Text </p>"
+document.body.appendChild(div)
