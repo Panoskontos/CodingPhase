c42f491974d74a261c2ae20d61a7cba7c9120593
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-03-20 23:06:53 +0200

    new

--- Practical Javascript/basics/index.js
+++ Practical Javascript/basics/index.js
@@ -7,9 +7,27 @@ document.body.style.padding = 0
 // using js instead of html css
 let div = document.createElement('div') 
 div.className = "js-box"
-div.style.width = '300px'
-div.style.height = '200px'
+div.style.width = '30px'
+div.style.height = '20px'
 div.style.background = 'black'
 div.style.color = "white"
 div.innerHTML = "<p> Text </p>"
 document.body.appendChild(div)
+
+// fuction that creates div
+function createDiv(size){
+
+    var div = document.createElement('div') 
+    div.className = "function-div"
+    div.style.width = size + 'px'
+    div.style.height = size + 'px'
+    div.style.background = 'black'
+    div.style.color = "white"
+    div.innerHTML = "<p> Text </p>"
+    document.body.appendChild(div)
+    
+}
+
+createDiv(50)
+createDiv(150)
+
